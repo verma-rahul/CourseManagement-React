3ade5c420dafb4b96135b72149d41c991b3a2513
Rahul Verma <rahulverma@ccs.neu.edu>
2018-03-18 22:27:33 -0400

    added proxy config for local and Heroku

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Home from '../Home/home'
 class App extends Component {
   render() {
     return (
       <div>
       Welcome to Course Management, Heroku Deploy Branch !!
+      <Home></Home>
       </div>
     );
   }
