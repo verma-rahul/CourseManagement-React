59bdb43260de9acf67d035a2085b0c853a2c7c8b
Rahul Verma <rahulverma@ccs.neu.edu>
2018-04-25 00:13:06 -0400

    widget baic show & hide done, with api and render left-  ----2

---
+++ src/constants/common.js
@@ -0,0 +1,6 @@
+export const WIDGET_OPTIONS = [
+  { key: 'IMAGE', text: 'IMAGE', value: 'IMAGE' },
+  { key: 'HTML', text: 'HTML', value: 'HTML' },
+  { key: 'TEXT', text: 'TEXT', value: 'TEXT' },
+  { key: 'VIDEO', text: 'VIDEO', value: 'VIDEO' },
+]
