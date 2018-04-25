3840f3271ef474e6438370dabcdf6ca6ebc78884
Rahul Verma <rahulverma@ccs.neu.edu>
2018-04-24 21:00:28 -0400

    help file inclued for widgets chapters

---
+++ src/components/course/help.js
@@ -0,0 +1,14 @@
+// Image
+<Segment basic >
+   <Header as='h1'>Example Image</Header>
+<Image centered  src='http://cdn.newsapi.com.au/image/v1/eb2c300a22b064cb3843313360341728' size='medium' rounded />
+</Segment>
+
+// Video
+<Segment basic size="mini" >
+<Header as='h1'>Example Video</Header>
+<Embed
+   id='O6Xo21L0ybE'
+   source='youtube'
+   aspectRatio="21:9"
+ />
